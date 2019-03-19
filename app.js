1b9a926b6d92ca0b560c38bed7dee8787eabc12c
tflin <tflins@163.com>
2019-03-19 21:20:59 +0800

    注册接口

--- app.js
+++ app.js
@@ -39,3 +39,4 @@ const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`监听${port}端口中`)
 })
+
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = new Router()
 const User = require('../../models/User')
 const gravatar = require('gravatar')
 const tools = require('../../config/tools')
+const bcrypt = require('bcryptjs')
 
 /**
  * @router GET api/users/test
@@ -50,6 +51,37 @@ router.post('/register', async ctx => {
       })
     // 返回json数据
     ctx.body = newUser
+  }
+})
+
+/**
+ * @router GET api/users/login
+ * @desc 登录接口地址 返回token
+ * @access 接口是公开的
+ */
+router.post('/login', async ctx => {
+  // 查询当前登录邮箱是否在数据库中
+  const findResult = await User.find({ email: ctx.request.body.email })
+  if (!findResult.length) {
+    ctx.state = 404
+    ctx.body = { email: '用户不存在！' }
+  } else {
+    // 查到后 验证密码
+    const result = await bcrypt.compareSync(
+      ctx.request.body.password,
+      findResult[0].password
+    )
+    if (result) {
+      ctx.state = 200
+      ctx.body = {
+        success: true
+      }
+    } else {
+      ctx.state = 400
+      ctx.body = {
+        password: '密码错误！'
+      }
+    }
   }
 })
 
